683b7f54fd99104d0ba94de22dacf238f0bf0407
Oleg <olegbackdev@gmail.com>
2025-04-29 18:23:55 +0300

    added logging functionality for CategroyController.

--- src/main/java/todo/list/todo_list/controller/CategoryController.java
+++ src/main/java/todo/list/todo_list/controller/CategoryController.java
@@ -2,6 +2,8 @@ package todo.list.todo_list.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +26,7 @@ import todo.list.todo_list.service.CategoryService;
 @RequestMapping("/api/tasks/categories")
 public class CategoryController {
 
+    private static final Logger log = LoggerFactory.getLogger(CategoryController.class);
     @Autowired
     private final CategoryService categoryService;
 
@@ -34,39 +37,105 @@ public class CategoryController {
     @GetMapping("/")
     @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
     public ResponseEntity<List<CategoryDTO>> getAllCategories() {
-        List<CategoryDTO> categories = categoryService.getAllCategories();
+        log.debug("Received request to get All Categories");
+         try {
+            ResponseEntity<List<CategoryDTO>> categoriesList = this.getAllCategoriesList();
 
-        return new ResponseEntity<>(categories, HttpStatus.OK);
+            return categoriesList;
+        } catch (Exception e) {
+            log.error("Get All Categories request failed", e);
+            throw e;
+        }
     }
 
     @PostMapping("/")
     @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
-    public ResponseEntity<CategoryDTO> createCategory(@Valid @RequestBody CategoryRequest request) {
-        CategoryDTO category = categoryService.createCategory(request);
+    public ResponseEntity<CategoryDTO> createCategory(@Valid @RequestBody CategoryRequest categoryRequest) {
+        log.debug("Received request to Create New Category");
+         try {
+            ResponseEntity<CategoryDTO> createdCategory = this.creatingCategory(categoryRequest);
 
-        return new ResponseEntity<>(category, HttpStatus.CREATED);
+            return createdCategory;
+        } catch (Exception e) {
+            log.error("Create New Category request failed", e);
+            throw e;
+        }
     }
 
     @GetMapping("/{categoryId}")
     @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
     public ResponseEntity<CategoryDTO> getCategoryById(@PathVariable Long categoryId) {
-        CategoryDTO category = categoryService.getCategory(categoryId);
+        log.debug("Received request to GET Category by categoryId: {}", categoryId);
+         try {
+            ResponseEntity<CategoryDTO> category = this.gettingCategory(categoryId);
 
-        return new ResponseEntity<>(category, HttpStatus.OK);
+            return category;
+        } catch (Exception e) {
+            log.error("Get Category by ID: {} request failed", categoryId, e);
+            throw e;
+        }
     }
 
     @PutMapping("/{categoryId}")
     @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
     public ResponseEntity<CategoryDTO> updateCategory(@PathVariable Long categoryId, @Valid @RequestBody CategoryRequest request) {
-        CategoryDTO existedCategory = categoryService.updateCategory(categoryId, request);
+        log.debug("Received request to UPDATE Category by ID: {}", categoryId);
+         try {
+            ResponseEntity<CategoryDTO> updatedCategory = this.updatingCategory(categoryId, request);
 
-        return new ResponseEntity<>(existedCategory, HttpStatus.OK);
+            return updatedCategory;
+        } catch (Exception e) {
+            log.error("Update Category by ID: {} request failed", categoryId, e);
+            throw e;
+        }
+        
     }
 
     @DeleteMapping("/{categoryId}")
     @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
-    public ResponseEntity<CategoryDTO> deleteCategory(@PathVariable Long categoryId) {
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long categoryId) {
+        log.debug("Received request to DELETE Category by ID: {}", categoryId);
+         try {
+            ResponseEntity<Void> response = this.deletingCategory(categoryId);
+            
+            return response;
+        } catch (Exception e) {
+            log.error("Update Category by ID: {} request failed", categoryId, e);
+            throw e;
+        }
+    }
+
+    private ResponseEntity<List<CategoryDTO>> getAllCategoriesList() {
+        List<CategoryDTO> categories = categoryService.getAllCategories();
+        log.info("Successfully Retreived List of Categories");
+
+        return new ResponseEntity<>(categories, HttpStatus.OK);
+    }
+
+    private ResponseEntity<CategoryDTO> creatingCategory(CategoryRequest categoryRequest) {
+        CategoryDTO category = categoryService.createCategory(categoryRequest);
+        log.info("Successfully Created Category");
+
+        return new ResponseEntity<>(category, HttpStatus.CREATED);
+    }
+
+    private ResponseEntity<CategoryDTO> gettingCategory(Long categoryId) {
+        CategoryDTO category = categoryService.getCategory(categoryId);
+        log.info("Successfully got Category by ID: {}", categoryId);
+
+        return new ResponseEntity<>(category, HttpStatus.OK);
+    }
+
+    private ResponseEntity<CategoryDTO> updatingCategory(Long categoryId, CategoryRequest request) {
+        CategoryDTO existedCategory = categoryService.updateCategory(categoryId, request);
+        log.info("Successfully Updated Category by ID: {}", categoryId);
+
+        return new ResponseEntity<>(existedCategory, HttpStatus.OK);
+    }
+
+    private ResponseEntity<Void> deletingCategory(Long categoryId) {
         categoryService.deleteCategory(categoryId);
+        log.info("Successfully Deleted Category by ID: {}", categoryId);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
